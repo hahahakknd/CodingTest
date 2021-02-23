dad9e30d609d4136da075e513a51ad0e4ebfe2a0
hahahakknd <hahahakknd@gmail.com>
2021-02-23 15:46:40 +0900

    15989 Success code

--- baekjoon/15989/plus.py
+++ baekjoon/15989/plus.py
@@ -1,4 +1,18 @@
 import sys
 
+def solution(number: int, max_num: int) -> int:
+    if max_num == 2:
+        return (number//max_num) + 1
+
+    count: int = 0
+    for i in range(0, (number//max_num)+1):
+        count += solution(number-(max_num*i), max_num-1)
+
+    return count
+
+# Input 오류는 고려하지 않는다.
 if __name__ == '__main__':
-    pass
+    loop_count: int = int(input())
+    for i in range(0, loop_count):
+        num: int = int(input())
+        print(str(solution(num, 3)))
--- baekjoon/4673/self_number.py
+++ baekjoon/4673/self_number.py
@@ -1,4 +1,5 @@
 import sys
 
+# Input 오류는 고려하지 않는다.
 if __name__ == '__main__':
     pass
