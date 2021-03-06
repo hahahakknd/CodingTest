d2e9a50e80d3b2390c095dbd32a189b86fd0cff6
hahahakknd <hahahakknd@gmail.com>
2021-03-06 21:20:58 +0900

    11654 Success code

--- baekjoon/11654/ascii.py
+++ baekjoon/11654/ascii.py
@@ -1,10 +1,7 @@
-def solution(schedule: list) -> None:
+def solution(char: str) -> None:
+    print(ord(char))
     return
 
 # Input 오류는 고려하지 않는다.
 if __name__ == '__main__':
-    input_schedule: list = []
-    loop_count: int = int(input())
-    for _ in range(0, loop_count):
-        input_schedule.append([int(x) for x in input().split(' ')])
-    solution(input_schedule)
+    solution(input())
