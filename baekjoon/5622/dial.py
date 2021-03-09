1a8a0916bdad0186a555dfb56c768d88aaa79f40
hahahakknd <hahahakknd@gmail.com>
2021-03-09 23:35:23 +0900

    5622 Success code

--- baekjoon/5622/dial.py
+++ baekjoon/5622/dial.py
@@ -1,13 +1,24 @@
-def solution() -> None:
+def solution(chars) -> None:
+    dials = [['A','B','C'],
+             ['D','E','F'],
+             ['G','H','I'],
+             ['J','K','L'],
+             ['M','N','O'],
+             ['P','Q','R','S'],
+             ['T','U','V'],
+             ['W','X','Y','Z']]
+    time = 0
+    for c in chars:
+        for i, dial in enumerate(dials):
+            try:
+                dial.index(c)
+                time += (i+3)
+                break
+            except ValueError:
+                pass
+    print(time)
     return
 
 # Input 오류는 고려하지 않는다.
 if __name__ == '__main__':
-    input_values: list = []
-    while True:
-        try:
-            A: int = int(input())
-            input_values.append(A)
-        except EOFError:
-            break
-    solution()
+    solution(input())
