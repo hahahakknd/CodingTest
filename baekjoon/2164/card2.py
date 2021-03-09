3a4511e7d2acac60d1008d8a2d7e33a5a3f069e8
hahahakknd <hahahakknd@gmail.com>
2021-03-10 00:40:12 +0900

    2164 Success code

--- baekjoon/2164/card2.py
+++ baekjoon/2164/card2.py
@@ -1,13 +1,37 @@
-def solution() -> None:
+def solution(number) -> None:
+    if number == 1:
+        print(1)
+        return
+
+    is_odd = (number%2 == 1)
+    org_list = []
+    if is_odd:
+        modified_list = [x for x in range(4,number+1,2)]
+        modified_list.append(2)
+    else:
+        modified_list = [x for x in range(2,number+1,2)]
+
+    while True:
+        if len(modified_list) == 1:
+            print(modified_list[0])
+            return
+
+        is_odd = (len(modified_list)%2 == 1)
+        org_list.clear()
+        org_list.extend(modified_list)
+        modified_list.clear()
+
+        if is_odd:
+            if len(org_list) >= 4:
+                for x in range(3,len(org_list),2):
+                    modified_list.append(org_list[x])
+            modified_list.append(org_list[1])
+        else:
+            if len(org_list) >= 2:
+                for x in range(1,len(org_list),2):
+                    modified_list.append(org_list[x])
     return
 
 # Input 오류는 고려하지 않는다.
 if __name__ == '__main__':
-    input_values: list = []
-    while True:
-        try:
-            A: int = int(input())
-            input_values.append(A)
-        except EOFError:
-            break
-    solution()
+    solution(int(input()))
