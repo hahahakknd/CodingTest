690e8d91dae2d067b1bc60ccfd9d6956a5979c24
hahahakknd <hahahakknd@gmail.com>
2021-03-19 15:43:22 +0900

    2579 Fail code

--- baekjoon/2579/up_stair2.py
+++ baekjoon/2579/up_stair2.py
@@ -1,39 +1,26 @@
-# 트리를 만들면 메모리 초과나 시간초과가 발생한다...
-# 다른방법이 필요한 듯...
+# 이것도 시간초과가 발생한다...
+# Stack 을 쓰는 방법은 시간초과가 날 수밖에 없나???
 class Node:
-    def __init__(self, parent, number, value) -> None:
-        self.parent = parent
+    def __init__(self, parent_number, parent_max_num, number, value) -> None:
+        self.parent_number = parent_number
         self.number = number
         self.value = value
-        self.max_sum = value
-        if parent is not None:
-            self.max_sum += parent.max_sum
-
-    def to_string(self) -> str:
-        msg = 'parent:'
-        if self.parent is None:
-            msg += 'None'
-        else:
-            msg += str(self.parent.number)
-        msg += ', number:' + str(self.number)
-        msg += ', value:'+str(self.value)
-        msg += ', max_num:'+str(self.max_sum)
-        return msg
+        self.max_sum = parent_max_num + value
 
 def solution() -> None:
     stairs_size = len(stairs)
     stack = []
-    stack.append(Node(None,0,stairs[0]))
+    stack.append(Node(-1,0,0,stairs[0]))
     max_sum = 0
 
     while True:
         try:
             node = stack.pop()
             if node.number+2 < stairs_size:
-                stack.append(Node(node,node.number+2,stairs[node.number+2]))
+                stack.append(Node(node.number, node.max_sum, node.number+2, stairs[node.number+2]))
             if node.number+1 < stairs_size:
-                if node.parent is None or node.parent.number != node.number-1:
-                    stack.append(Node(node,node.number+1,stairs[node.number+1]))
+                if node.parent_number == -1 or node.parent_number != node.number-1:
+                    stack.append(Node(node.number, node.max_sum, node.number+1, stairs[node.number+1]))
             if node.number+1 == stairs_size and max_sum < node.max_sum:
                 max_sum = node.max_sum
         except IndexError:
