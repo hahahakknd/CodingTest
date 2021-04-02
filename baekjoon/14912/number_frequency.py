85081a7fbf7d59065682e664cadef12fecb0cb41
hahahakknd <hahahakknd@gmail.com>
2021-04-02 17:28:20 +0900

    14912 Success code

--- baekjoon/14912/number_frequency.py
+++ baekjoon/14912/number_frequency.py
@@ -1,12 +1,15 @@
-def solution() -> None:
+def solution(max_num, find_num) -> None:
+    count = 0
+    for i in range(1, max_num+1):
+        for j in str(i):
+            if int(j) == find_num:
+                count += 1
+    print(count)
     return
 
 # Input 오류는 고려하지 않는다.
 if __name__ == '__main__':
-    stairs = []
-    loop_count = int(input())
-    for _ in range(0, loop_count):
-        stairs.append(int(input()))
-    solution()
-    # stairs = [10,20,15,25,10,20]
-    # solution()
+    inputs = input().split(" ")
+    solution(int(inputs[0]), int(inputs[1]))
+    # solution(11, 1)
+    # solution(100, 3)
