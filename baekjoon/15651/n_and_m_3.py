7db1da8fa008b102be5eef6c5ac18270fec283fc
hahahakknd <hahahakknd@gmail.com>
2021-04-04 23:41:30 +0900

    15651 Success code

--- baekjoon/15651/n_and_m_3.py
+++ baekjoon/15651/n_and_m_3.py
@@ -1,12 +1,26 @@
-def solution() -> None:
+def make_str(seq_num) -> str:
+    result = str(seq_num[0])
+    for x in range(1,len(seq_num)):
+        result += " " + str(seq_num[x])
+    return result
+
+def solution(seq_num:list, n, m) -> None:
+    for j in range(1,10):
+        if j > n:
+            break
+        if m == 1:
+            seq_num.append(j)
+            print(make_str(seq_num))
+        else:
+            seq_num.append(j)
+            solution(seq_num, n, m-1)
+        seq_num.pop()
     return
 
 # Input 오류는 고려하지 않는다.
 if __name__ == '__main__':
-    stairs = []
-    loop_count = int(input())
-    for _ in range(0, loop_count):
-        stairs.append(int(input()))
-    solution()
-    # stairs = [10,20,15,25,10,20]
-    # solution()
+    input_data = input().split(" ")
+    solution([], int(input_data[0]), int(input_data[1]))
+    # solution([], 3, 1)
+    # solution([], 4, 2)
+    # solution([], 3, 3)
