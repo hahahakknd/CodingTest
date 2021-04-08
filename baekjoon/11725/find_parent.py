adf17ccffe007fb9d840614bc0b11497e703818c
hahahakknd <hahahakknd@gmail.com>
2021-04-09 05:13:35 +0900

    11752 Temp code

--- baekjoon/11725/find_parent.py
+++ baekjoon/11725/find_parent.py
@@ -1,89 +1,89 @@
-def next_index(index) -> int:
-    if index == len(LINKS)-1:
-        return 0
-    else:
-        return index + 1
+import sys
+
+def what_is_root(node) -> int:
+    global NODES, STACK
+    up_node = NODES[node]
+    before_node = -1
+    while True:
+        if up_node == 0:
+            return 0
+        if up_node == 1:
+            return 1
+        if NODES[up_node] == before_node:
+            return 0
+        before_node = up_node
+        up_node = NODES[up_node]
+
+def find_parent(node_1, node_2) -> None:
+    global NODES, STACK
+    if node_1 == 1:
+        NODES[node_2] = node_1
+        return
+    if node_2 == 1:
+        NODES[node_1] = node_2
+        return
+    if what_is_root(node_1) == 0:
+        NODES[node_1] = node_2
+    if what_is_root(node_2) == 0:
+        NODES[node_2] = node_1
 
 def solution() -> None:
-    index = 0
-    match_parent = 0
-    while len(LINKS) != match_parent:
-        link = LINKS[index]
-        node_1 = link[0]
-        if node_1 == -1:
-            index = next_index(index)
-            continue
-        node_2 = link[1]
-        if node_1 == 1:
-            NODES[node_2] = node_1
-            LINKS[index][0] = -1
-            match_parent += 1
-        elif node_2 == 1:
-            NODES[node_1] = node_2
-            LINKS[index][0] = -1
-            match_parent += 1
-        elif NODES.get(node_1) != 0:
-            NODES[node_2] = node_1
-            LINKS[index][0] = -1
-            match_parent += 1
-        elif NODES.get(node_2) != 0:
-            NODES[node_1] = node_2
-            LINKS[index][0] = -1
-            match_parent += 1
-        index = next_index(index)
-
+    global NODES, STACK
     for parent in NODES.values():
         if parent != 0:
             print(parent)
-    return
 
 # Input 오류는 고려하지 않는다.
 if __name__ == '__main__':
-    global NODES, LINKS
+    global NODES, STACK
     NODES = {}
-    LINKS = []
-    node_count = int(input())
-    NODES[1] = 0
+    STACK = []
+    node_count = int(sys.stdin.readline().rstrip())
+    for i in range(1, node_count+1):
+        NODES[i] = 0
     for i in range(1, node_count):
-        NODES[i+1] = 0
-        input_data = input().split(' ')
-        LINKS.append([int(input_data[0]), int(input_data[1])])
+        input_data = sys.stdin.readline().rstrip().split(' ')
+        find_parent(int(input_data[0]), int(input_data[1]))
     solution()
     # for i in range(1,7+1):
     #     NODES[i] = 0
-    # LINKS.append([1,6])
-    # LINKS.append([6,3])
-    # LINKS.append([3,5])
-    # LINKS.append([4,1])
-    # LINKS.append([2,4])
-    # LINKS.append([4,7])
+    # find_parent(1,6)
+    # find_parent(6,3)
+    # find_parent(3,5)
+    # find_parent(4,1)
+    # find_parent(2,4)
+    # find_parent(4,7)
     # solution()
     # print('----------------')
     # NODES.clear()
-    # LINKS.clear()
-    # for i in range(1,7+1):
+    # STACK.clear()
+    # for i in range(1,11+1):
     #     NODES[i] = 0
-    # LINKS.append([6,3])
-    # LINKS.append([4,7])
-    # LINKS.append([4,1])
-    # LINKS.append([1,6])
-    # LINKS.append([2,4])
-    # LINKS.append([3,5])
+    # find_parent(1,3)
+    # find_parent(9,10)
+    # find_parent(1,9)
+    # find_parent(3,7)
+    # find_parent(3,4)
+    # find_parent(7,6)
+    # find_parent(2,11)
+    # find_parent(5,2)
+    # find_parent(7,5)
+    # find_parent(7,8)
     # solution()
     # print('----------------')
     # NODES.clear()
-    # LINKS.clear()
+    # STACK.clear()
     # for i in range(1,12+1):
     #     NODES[i] = 0
-    # LINKS.append([1,2])
-    # LINKS.append([1,3])
-    # LINKS.append([2,4])
-    # LINKS.append([3,5])
-    # LINKS.append([3,6])
-    # LINKS.append([4,7])
-    # LINKS.append([4,8])
-    # LINKS.append([5,9])
-    # LINKS.append([5,10])
-    # LINKS.append([6,11])
-    # LINKS.append([6,12])
+    # find_parent(1,2)
+    # find_parent(1,3)
+    # find_parent(2,4)
+    # find_parent(3,5)
+    # find_parent(3,6)
+    # find_parent(4,7)
+    # find_parent(4,8)
+    # find_parent(5,9)
+    # find_parent(5,10)
+    # find_parent(6,11)
+    # find_parent(6,12)
     # solution()
