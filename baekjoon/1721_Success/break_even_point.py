b2d9dafef2668576e79ad2e11dbfe52d0bd31358
hahahakknd <hahahakknd@gmail.com>
2022-07-04 09:33:02 +0900

    1721번 문제에 부동소수점 내용 추가

--- baekjoon/1721_Success/break_even_point.py
+++ baekjoon/1721_Success/break_even_point.py
@@ -3,6 +3,7 @@ import sys
 # 21억 이하의 자연수라는 의미는 int32 를 input 으로 받는다는 의미이다.
 # 고정비용: A, 가변비용: B, 노트북가격: C, 판매대수: D
 # C*D = A+B*D --> (C*D)-(B*D) = A --> (C-B)*D = A --> D = A/(C-B)
+# Python 에서 '/' 는 부동소수점 나눗셈이고, '//' 는 정수 나눗셈이다.
 def solution(fixed_cost: int, variable_cost: int, notebook_cost: int) -> None:
     # denominator 는 분모라는 의미
     denominator: int = notebook_cost - variable_cost
@@ -12,10 +13,10 @@ def solution(fixed_cost: int, variable_cost: int, notebook_cost: int) -> None:
         print("-1")
         return
 
-    result: float = fixed_cost / denominator
+    result: int = fixed_cost // denominator
 
     # 공식의 결과보다 무조건 1대를 더 팔아야 손익분기점이다.
-    sales: int = int(result+1)
+    sales: int = result + 1
 
     print(sales)
     return
