abf6c41b4fac8ef4d3d07862ed48f7dfedcdc7aa
hahahakknd <hahahakknd@gmail.com>
2022-07-25 11:52:53 +0900

    백준문제 업로드

---
+++ baekjoon/1929/get_prime_number.py
@@ -0,0 +1,9 @@
+import sys
+
+def solution() -> None:
+
+
+# Input 오류는 고려하지 않는다.
+if __name__ == '__main__':
+    input_data = sys.stdin.readline().rstrip().split(' ')
+    solution(int(input_data[0]))
